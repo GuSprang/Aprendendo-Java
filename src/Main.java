f7fdadc72a2d090c65fea4b9d4eed6f1b4326a79
Gustavo de Araujo Sprangoski <gustavosprangoski@umc.br>
2025-10-24 12:51:31 -0300

    primeiro-commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) { //Método main, por onde o programa começará a ser lido
+    System.out.println("Hello World");   // Comando para exibir mensagens no console
+    // Tipos de Variáveis
+    byte numeroInteiroP = 8;             // byte é uma váriavel que comporta apenas números inteiros de ate 8 bits
+    short numeroInteiroM = 16;           // byte é uma váriavel que comporta apenas números inteiros de ate 16 bits
+    int numeroInteiro = 10;              // int é uma variável que comporta apenas números inteiros de até 32 bits
+    long numeroInteiroGG = 233;          // long é uma váriavel que comporta apenas números inteiso de até 64 bits
+    double numeroDecimal = 3.1415;       // double é uma variável que comporta números inteiros e decimais, porém números inteiros adicionados a essa variável receberão um ".0" EX: 10 - 10.0
+    boolean verdadeiro = true;           // boolean é uma variável que comporta apenas "true" para verdadeiro e "falso" para falso
+    String texto = "Ola!";               //String é uma "variável" que comporta apenas textos
+
+
+
+}
